99b7d450fb86832b1762657d0ce30df4388a0e44
Stephan Lotter <Stephan@neurasoft.co.za>
2023-11-16 08:07:31 +0200

    Simplified FindWarehouseCode to only return the default warehouse.

--- src/App/BosmanCommerce7/BosmanCommerce7.Module/ApplicationServices/DataAccess/LocalDatabaseDataAccess/WarehouseRepository.cs
+++ src/App/BosmanCommerce7/BosmanCommerce7.Module/ApplicationServices/DataAccess/LocalDatabaseDataAccess/WarehouseRepository.cs
@@ -1,20 +1,19 @@
-﻿/* 
+﻿/*
  * Copyright (C) Neurasoft Consulting cc.  All rights reserved.
  * www.neurasoft.co.za
  * Date created: 2023-08-21
  * Author	: Stephan J Lotter
- * Notes	: 
- *  
+ * Notes	:
+ *
  */
 
-using BosmanCommerce7.Module.BusinessObjects;
-using BosmanCommerce7.Module.Extensions;
 using BosmanCommerce7.Module.Models;
 using BosmanCommerce7.Module.Models.LocalDatabase;
 using CSharpFunctionalExtensions;
 using Microsoft.Extensions.Logging;
 
 namespace BosmanCommerce7.Module.ApplicationServices.DataAccess.LocalDatabaseDataAccess {
+
   public class WarehouseRepository : LocalDatabaseRepositoryBase, IWarehouseRepository {
     private readonly IValueStoreRepository _valueStoreRepository;
 
@@ -23,19 +22,18 @@ namespace BosmanCommerce7.Module.ApplicationServices.DataAccess.LocalDatabaseDat
     }
 
     public Result<string?> FindWarehouseCode(FindWarehouseCodeDescriptor findWarehouseCodeDescriptor) {
+      return _valueStoreRepository.GetValue("default-ship-from-warehouse-code");
 
-      if (!findWarehouseCodeDescriptor.IsStoreOrder) {
-        return _valueStoreRepository.GetValue("default-ship-from-warehouse-code");
-      }
+      //if (!findWarehouseCodeDescriptor.IsStoreOrder) {
+      //  return _valueStoreRepository.GetValue("default-ship-from-warehouse-code");
+      //}
 
-      if (string.IsNullOrWhiteSpace(findWarehouseCodeDescriptor.PostalCode)) {
-        return Result.Failure<string?>("Postal code not provided");
-      }
+      //if (string.IsNullOrWhiteSpace(findWarehouseCodeDescriptor.PostalCode)) {
+      //  return Result.Failure<string?>("Postal code not provided");
+      //}
 
-      var mapping = findWarehouseCodeDescriptor.ObjectSpace.FindObject<WarehousePostalCodeMapping>("PostalCode".IsEqualToOperator(findWarehouseCodeDescriptor.PostalCode.Trim()));
-      return mapping != null ? Result.Success(mapping.WarehouseCode) : Result.Failure<string?>($"No postal code/warehouse code mapping found for {findWarehouseCodeDescriptor.PostalCode.Trim()}.");
+      //var mapping = findWarehouseCodeDescriptor.ObjectSpace.FindObject<WarehousePostalCodeMapping>("PostalCode".IsEqualToOperator(findWarehouseCodeDescriptor.PostalCode.Trim()));
+      //return mapping != null ? Result.Success(mapping.WarehouseCode) : Result.Failure<string?>($"No postal code/warehouse code mapping found for {findWarehouseCodeDescriptor.PostalCode.Trim()}.");
     }
-
   }
-
 }
