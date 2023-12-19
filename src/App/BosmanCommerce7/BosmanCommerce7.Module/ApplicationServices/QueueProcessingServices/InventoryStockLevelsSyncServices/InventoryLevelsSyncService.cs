927201bf3f464374b9818746ff519be5c14a4098
Stephan Lotter <Stephan@neurasoft.co.za>
2023-12-19 10:25:14 +0200

    Added InventoryLevelsSyncService stubs

--- src/App/BosmanCommerce7/BosmanCommerce7.Module/ApplicationServices/QueueProcessingServices/InventoryStockLevelsSyncServices/InventoryLevelsSyncService.cs
+++ src/App/BosmanCommerce7/BosmanCommerce7.Module/ApplicationServices/QueueProcessingServices/InventoryStockLevelsSyncServices/InventoryLevelsSyncService.cs
@@ -7,15 +7,50 @@
  *
  */
 
+using BosmanCommerce7.Module.ApplicationServices.DataAccess.LocalDatabaseDataAccess;
+using BosmanCommerce7.Module.ApplicationServices.QueueProcessingServices.CustomerMasterSyncServices.Models;
 using BosmanCommerce7.Module.ApplicationServices.QueueProcessingServices.InventoryItemsSyncServices.Models;
+using BosmanCommerce7.Module.ApplicationServices.QueueProcessingServices.InventoryItemsSyncServices.RestApi;
+using BosmanCommerce7.Module.BusinessObjects;
+using BosmanCommerce7.Module.BusinessObjects.Customers;
 using CSharpFunctionalExtensions;
+using Microsoft.Extensions.Logging;
 
 namespace BosmanCommerce7.Module.ApplicationServices.QueueProcessingServices.InventorySyncServices {
 
-  public class InventoryLevelsSyncService : IInventoryLevelsSyncService {
+  public class InventoryLevelsSyncService : SyncMasterDataServiceBase, IInventoryLevelsSyncService {
+    private readonly IInventoryItemApiClient _inventoryItemApiClient;
+    private readonly IInventoryItemsLocalCache _inventoryItemsLocalCache;
+
+    public InventoryLevelsSyncService(ILogger<InventoryLevelsSyncService> logger,
+      ILocalObjectSpaceEvolutionSdkProvider localObjectSpaceEvolutionSdkProvider,
+      IInventoryItemApiClient inventoryItemApiClient,
+      IInventoryItemsLocalCache inventoryItemsLocalCache) :
+      base(logger, localObjectSpaceEvolutionSdkProvider) {
+      _inventoryItemApiClient = inventoryItemApiClient;
+      _inventoryItemsLocalCache = inventoryItemsLocalCache;
+    }
 
     public Result<InventoryLevelsSyncResult> Execute(InventoryLevelsSyncContext context) {
-      throw new System.NotImplementedException();
+      _errorCount = 0;
+      _processedIds.Clear();
+
+      Logger.LogDebug("Start {SyncService} records sync.", this.GetType().Name);
+
+      return ProcessQueueItems<CustomerUpdateQueue>(context)
+        .Bind(() => BuildResult())
+        .Finally(result => {
+          Logger.LogDebug("End {SyncService} records sync.", this.GetType().Name);
+          return result;
+        });
+
+      Result<CustomerMasterSyncResult> BuildResult() {
+        return _errorCount == 0 ? Result.Success(new CustomerMasterSyncResult { }) : Result.Failure<CustomerMasterSyncResult>($"Completed with {_errorCount} errors.");
+      }
+    }
+
+    protected override Result ProcessQueueItem(UpdateQueueBase updateQueueItem) {
+      throw new NotImplementedException();
     }
   }
 }
