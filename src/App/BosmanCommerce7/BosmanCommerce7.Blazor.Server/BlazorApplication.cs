a45881be1a4233f99062abf64152d1dd29539988
Stephan Lotter <Stephan@neurasoft.co.za>
2024-05-09 11:19:32 +0200

    Enabled auto DB migration.

--- src/App/BosmanCommerce7/BosmanCommerce7.Blazor.Server/BlazorApplication.cs
+++ src/App/BosmanCommerce7/BosmanCommerce7.Blazor.Server/BlazorApplication.cs
@@ -1,10 +1,10 @@
-﻿/* 
+﻿/*
  * Copyright (C) Neurasoft Consulting cc.  All rights reserved.
  * www.neurasoft.co.za
  * Date created: 2023-08-14
  * Author	: Stephan J Lotter
- * Notes	: 
- *  
+ * Notes	:
+ *
  */
 
 using DevExpress.ExpressApp;
@@ -30,27 +30,7 @@ public class BosmanCommerce7BlazorApplication : BlazorApplication {
   }
 
   private void BosmanCommerce7BlazorApplication_DatabaseVersionMismatch(object? sender, DatabaseVersionMismatchEventArgs e) {
-#if EASYTEST
-        e.Updater.Update();
-        e.Handled = true;
-#else
-    if (System.Diagnostics.Debugger.IsAttached) {
-      e.Updater.Update();
-      e.Handled = true;
-    }
-    else {
-      string message = "The application cannot connect to the specified database, " +
-          "because the database doesn't exist, its version is older " +
-          "than that of the application or its schema does not match " +
-          "the ORM data model structure. To avoid this error, use one " +
-          "of the solutions from the https://www.devexpress.com/kb=T367835 KB Article.";
-
-      if (e.CompatibilityError != null && e.CompatibilityError.Exception != null) {
-        message += "\r\n\r\nInner exception: " + e.CompatibilityError.Exception.Message;
-      }
-      throw new InvalidOperationException(message);
-    }
-#endif
+    e.Updater.Update();
+    e.Handled = true;
   }
-
 }
